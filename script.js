d46bf7ebd1ae3ec2de80d719011003d3170dc31a
FREEMAN-LT\stepa <stepansidorov3234@gmail.com>
2024-02-26 18:40:03 +0300

    ...

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let menuIconEl=document.querySelector('#menu-icon');
 let navbarEl=document.querySelector('.navbar');
 let sectionsEls=document.querySelectorAll('section');
 let navLinksEls=document.querySelectorAll('header nav a');
+let fromEmailEl=document.getElementById("email").value;
+
 
 window.onscroll=()=>{
     sectionsEls.forEach(sec=>{
@@ -19,7 +21,23 @@ window.onscroll=()=>{
     })
 }
 
+
 menuIconEl.onclick=()=>{
     menuIconEl.classList.toggle('fa-x');
     navbarEl.classList.toggle('active');
 }
+
+
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.gmail.com",
+//         Username : "stepansidorov3234@gmail.com",
+//         Password : "",
+//         To : 'stepansidorov3234@gmail.com',
+//         From : fromEmailEl,
+//         Subject : "New Contact Form Enquiry",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
